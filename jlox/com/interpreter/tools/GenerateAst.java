428f0ef20a3bdef29f805c74643641ec983f8ec4
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2022-07-16 07:55:05 -0300

    implement visitor pattern

--- jlox/com/interpreter/tools/GenerateAst.java
+++ jlox/com/interpreter/tools/GenerateAst.java
@@ -29,9 +29,61 @@ public class GenerateAst {
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
         writer.println();
+
+        // AST Classes
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        // base accept() method
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("}");
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // store params in fields
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("        this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // visitor pattern
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
+
+        // fields
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+        writer.println();
+
+        // writer.println(" }");
     }
 }
